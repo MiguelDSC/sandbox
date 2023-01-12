96f815711154dd12fc9253516fe3ffd71911178a
MiguelDSC <miguel@hotmail.nl>
2023-01-12 14:45:15 +0100

    🚧 added user input

--- burgerking-questionnaire-filler/src/App.tsx
+++ burgerking-questionnaire-filler/src/App.tsx
@@ -1,7 +1,35 @@
+import { FormEvent, useState } from "react";
 import "./App.css";
+import styles from "./customCSS.module.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [enteredText, setEnteredText] = useState("");
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      {/* user input for  */}
+      <div className={styles.input}>
+        Enter the burgerking restaurant number
+        <form onSubmit={submitHandler}>
+          <input
+            type="number"
+            onChange={(e) => {
+              setEnteredText(e.target.value);
+            }}
+          />
+          <button>Generate code</button>
+        </form>
+      </div>
+
+      <h2></h2>
+
+      {/* show validation code */}
+    </div>
+  );
 }
 
 export default App;
---
+++ burgerking-questionnaire-filler/src/BurgerkingService.ts
@@ -0,0 +1,1 @@
+export const getBurgerKingValidationCode = (restaurantNumber: number) => {};
